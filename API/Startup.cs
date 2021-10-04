7d438c7778424df7c711eebf1fa67d6ed9011b3e
Hadi Daana <hhdaana@umich.edu>
2021-10-03 21:06:41 -0400

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnections"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -51,6 +52,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(p => p.AllowAnyHeader().AllowAnyHeader().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({ //decorator
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'The Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+}
